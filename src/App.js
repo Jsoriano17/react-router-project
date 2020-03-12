1bf8b08600716a6665af1ca74d30d3f36a0c9c29
Jeffrey Soriano <jeffsor17@gmail.com>
2020-03-12 15:44:43 -0600

    added first few components

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    viruses: [
+      {id: 1, name: 'COVID-19', about: 'boomer killer'},
+      {id: 2, name: 'Ebola', about: 'A virus that causes severe bleeding, organ failure, and can lead to death.'}
+    ],
+  }
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
 }
-
-export default App;
