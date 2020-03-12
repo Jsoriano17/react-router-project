7215ca22daf173aa1ef3510b4c9804db5ed8481c
Brianna Bell <bbell9563@gmail.com>
2020-03-12 15:45:28 -0600

    navbar made

---
+++ src/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Menu, Divider } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+export default class NavBar extends React.Component {
+  state = {
+    activeItem: 'home'
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu inverted>
+        <Link to="/">
+          <Menu.Item
+            as={Divider}
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          >
+            Home
+        </Menu.Item>
+        </Link>
+        <Link>
+          <Menu.Item
+            as={Divider}
+            name='about'
+            active={activeItem === 'about'}
+            onClick={this.handleItemClick}
+          >
+            About
+        </Menu.Item>
+        </Link>
+        <Link>
+          <Menu.Item
+            as={Divider}
+            name='virus'
+            active={activeItem === 'about'}
+            onClick={this.handleItemClick}
+          >
+            Virus
+        </Menu.Item>
+        </Link>
+
+      </Menu >
+    )
+  }
+}
